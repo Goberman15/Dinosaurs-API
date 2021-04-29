6a1ffa918449aee8d1a11bead206fc3ee2327e57
goberman15 <akbarrmdhn94@gmail.com>
2021-04-29 22:32:10 +0700

    add dino list by era, found place, body type, diet type, nalphabet

--- router.js
+++ router.js
@@ -3,9 +3,14 @@ const DinoController = require('./controllers/dinoController');
 
 router.get('/dinosaurs', DinoController.getAllDinoList);
 router.get('/dinosaurs/:name', DinoController.getDinoDataByName);
+router.get('/name/:alphabet', DinoController.getDinoListByName);
 router.get('/era', DinoController.getDinoEra);
+router.get('/era/:era', DinoController.getDinoListByEra);
 router.get('/found', DinoController.getDinoFoundPlace);
+router.get('/found/:found_place', DinoController.getDinoListByFoundPlace);
 router.get('/type', DinoController.getDinoType);
+router.get('/type/:body_type', DinoController.getDinoListByBodyType);
 router.get('/diet', DinoController.getDinoFood);
+router.get('/diet/:diet', DinoController.getDinoListByDietType);
 
 module.exports = router;
